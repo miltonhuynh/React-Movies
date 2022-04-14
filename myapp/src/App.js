9a5a421f22f162034d22abe5e4bdb08615772216
miltonhuynh <miltonhuynh@gmail.com>
2022-04-14 15:08:18 -0700

    This stuff ain't working mane

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,70 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MovieCards from './components/MovieCards.js';
 import {useState} from 'react';
-
-
-const movieData = {
-  Title: "The Avengers",
-  Year: "2012",
-  Rated: "PG-13",
-  Released: "04 May 2012",
-  Runtime: "143 min",
-  Genre: "Action, Adventure, Sci-Fi",
-  Director: "Joss Whedon",
-  Writer: "Joss Whedon, Zak Penn",
-  Actors: "Robert Downey Jr., Chris Evans, Scarlett Johansson",
-  Plot:
-    "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-  Language: "English, Russian, Hindi",
-  Country: "United States",
-  Awards: "Nominated for 1 Oscar. 38 wins & 80 nominations total",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-  Ratings: [
-    {
-      Source: "Internet Movie Database",
-      Value: "8.0/10"
-    },
-    {
-      Source: "Rotten Tomatoes",
-      Value: "91%"
-    },
-    {
-      Source: "Metacritic",
-      Value: "69/100"
-    }
-  ],
-  Metascore: "69",
-  imdbRating: "8.0",
-  imdbVotes: "1,339,955",
-  imdbID: "tt0848228",
-  Type: "movie",
-  DVD: "25 Sep 2012",
-  BoxOffice: "$623,357,910",
-  Production: "N/A",
-  Website: "N/A",
-  Response: "True"
-};
-
-
-//const [searchTerm, setsearchterm] = useState("batman");
-//const [isLoading, setisLoading] = useState(false);
-//const [movies, setmovies] = useState([]);
-//const [error, seterror] = useState(null);
+import MovieCards from './MovieCards.js';
+import GetMovieData from './GetMovieData.js';
 
 function App() {
   return (
-    <MovieCards 
-      posterUrl={movieData.Poster}
-      title={movieData.Title}
-      rated={movieData.Rated}
-      runtime={movieData.Runtime}
-      genre={movieData.Genre}
-      plot={movieData.Plot}
-      actors={movieData.Actors}
-      rating={movieData.imdbRating}
-    />
+    <>
+      <GetMovieData/>
+      <MovieCards 
+        posterUrl={GetMovieData.movieData.Poster}
+        title={GetMovieData.movieData.Title}
+        rated={GetMovieData.movieData.Rated}
+        runtime={GetMovieData.movieData.Runtime}
+        genre={GetMovieData.movieData.Genre}
+        plot={GetMovieData.movieData.Plot}
+        actors={GetMovieData.movieData.Actors}
+        rating={GetMovieData.movieData.imdbRating}
+      />
+    </>
   )
 }
 
--- myapp/src/MovieCards.js
+++ myapp/src/MovieCards.js
@@ -1,16 +1,18 @@
+import React from 'react'
+
 export default function MovieCards(props) {
     return (
     <div id="container">
-        <div class="image">
+        <div className="image">
             <img src={props.posterUrl}></img>
         </div>
         <div id="MovieInfo">
             <div id="MovieTitle">{props.title}</div>
             <div id="imdb">{props.rating}</div>
             <div id="Buttons">
-                <div class="GrayBg">{props.rated}</div>
-                <div class="GrayBg">{props.runtime}</div>
-                <div class="GrayBg">{props.genre}</div>
+                <div className="GrayBg">{props.rated}</div>
+                <div className="GrayBg">{props.runtime}</div>
+                <div className="GrayBg">{props.genre}</div>
             </div>
             <div><b>Plot</b><br/>{props.plot}</div>
             <div><b>Actors: </b><br/>{props.actors}</div>
