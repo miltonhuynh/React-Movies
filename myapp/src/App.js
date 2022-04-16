59ccf7a9cb1fe119eb4b3b54d252e9c99f01e532
miltonhuynh <miltonhuynh@gmail.com>
2022-04-15 17:36:45 -0700

    Switching computers

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,8 +7,13 @@ import MovieDetails from './components/MovieDetails.js';
 function App() {
 
   const [movieData, setmovieData] = useState({});
+  const [searchTerm, setsearchTerm] = useState("batman");
+  const [isLoading, setisLoading] = useState(false);
+  const [movies, setmovies] = useState([]);
+  const [error, seterror] = useState(null);
 
     useEffect(() => {
+        setisLoading(true);
         const url = "https://www.omdbapi.com/?apikey=62c9fe58&t=Avengers";
 
         const fetchData = async () => {
@@ -19,24 +24,30 @@ function App() {
                 setTimeout(function() {
                     setmovieData(data);
                 }, 1000);
+                setisLoading(false);
+                seterror(null);
             } catch (error) {
-                console.log("error", error);
+                seterror(error);
+                movies([]);
+                setisLoading(false);
             }
         };
         fetchData();
     }, []);
 
     return (
-        <MovieDetails 
-          posterUrl={movieData.Poster}
-          title={movieData.Title}
-          rated={movieData.Rated}
-          runtime={movieData.Runtime}
-          genre={movieData.Genre}
-          plot={movieData.Plot}
-          actors={movieData.Actors}
-          rating={movieData.imdbRating}
-        />
+        <>
+            <MovieDetails 
+            posterUrl={movieData.Poster}
+            title={movieData.Title}
+            rated={movieData.Rated}
+            runtime={movieData.Runtime}
+            genre={movieData.Genre}
+            plot={movieData.Plot}
+            actors={movieData.Actors}
+            rating={movieData.imdbRating}
+            />
+        </>
     );
 }
 
